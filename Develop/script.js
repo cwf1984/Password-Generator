29a2ab384ca397bd3ba3e0fa6e91f6be9f826568
Vanessa Malloy <catwoman.fan.84@gmail.com>
2020-10-23 11:23:15 -0700

    add .split to each array

--- Develop/script.js
+++ Develop/script.js
@@ -3,20 +3,21 @@ var generateBtn = document.querySelector("#generate");
 
 // Array list of numbers
 
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var numbers = "0123456789".split("");
 
 // Array list of lowercase letters
 
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
 
 // Array list of uppercase letters
 
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 
 // Array list of special characters
 
-var specialChars = ["@", "%", "+", "!", "#", "$", "?", "."];
+var specialChars = "@%+!#$?.".split("");
 
+var alphabetNumericCharacters = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
 
 
 function generatePassword () {
@@ -27,7 +28,7 @@ function generatePassword () {
 
   // IF user does not follow character conditions
 
-  if ( (passwordLength <8 || passwordLength > 128) ) {
+  if ( (passwordLength < 8 || passwordLength > 128) ) {
   alert ("I'm sorry, this does not meet our parameters.")
   return;
   }
